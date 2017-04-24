08eed95dbdab009f5d7700e55573f163edde6f76
Marcos <marcosmolero@mac-mini-de-marcos.local>
2017-04-24 13:26:41 +0200

    1.0.0 (1) - Progress in storyboard view.

--- habitissimochallenge/NewBudgetVC.swift
+++ habitissimochallenge/NewBudgetVC.swift
@@ -9,22 +9,46 @@
 import UIKit
 
 class NewBudgetVC: UIViewController {
-
+    
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var phoneField: UITextField!
+    @IBOutlet weak var descriptionField: UITextField!
+    @IBOutlet weak var subcategoryField: UITextField!
+    @IBOutlet weak var locationField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let wsc:WebServiceCommunication = WebServiceCommunication()
-//        wsc.getCategories()
-//        wsc.getSubcategory("001-1")
-//        wsc.getLocations()
-        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func cancelBudget(_ sender: Any) {
+        // Clear budget fields.
+        
+        nameField.text = ""
+        emailField.text = ""
+        phoneField.text = ""
+        descriptionField.text = ""
+        subcategoryField.text = ""
+        locationField.text = ""
+        
+    }
 
+    @IBAction func saveBudget(_ sender: Any) {
+        // Save in DB.
+        
+        
+        
+        
+    }
+    
+    @IBAction func dismissKeyboard(_ sender: Any) {
+        self.resignFirstResponder()
+    }
 
 }
 
